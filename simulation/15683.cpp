af251b32c14f0cfe4bf616d9301165e0b521e02e
mhlee <mhlee@autocrypt.io>
2024-12-19 22:47:56 +0900

    simulation-15683

--- simulation/15683.cpp
+++ simulation/15683.cpp
@@ -2,13 +2,11 @@
 using namespace std;
 
 int n, m;
-int min;
+int min_value = 68;
 char board[8][8];
+bool vis[8];
 // queue<tuple<int, int, char>> cctv_pos;
-queue<pair<int, int>> cctv1_pos;
-queue<pair<int, int>> cctv2_pos;
-queue<pair<int, int>> cctv3_pos;
-queue<pair<int, int>> cctv4_pos;
+vector<tuple<int, int, char>> cctv_pos;
 queue<pair<int, int>> cctv5_pos;
 
 void board_clear() {
@@ -98,6 +96,14 @@ void cctv_2_draw(int x, int y, int dir) {
        right_draw(x, y);
     }
     // up and down
+    else if (dir == 1) {
+        up_draw(x, y);
+        down_draw(x, y);
+    }
+    else if (dir == 2) {
+        left_draw(x, y);
+        right_draw(x, y);
+    }
     else {
         up_draw(x, y);
         down_draw(x, y);
@@ -156,10 +162,51 @@ void cctv_4_draw(int x, int y, int dir) {
 
 void cctv_5_draw(int x, int y, int dir) {
     // all dir
-    up_draw(x, y);
-    right_draw(x, y);
-    down_draw(x, y);
-    left_draw(x, y);
+
+    for (int i = (y+1); i < m; i++) {
+        if (board[x][i] == '6') break;
+        if (board[x][i] == '0') {
+            board[x][i] = '7';
+        }
+    }
+
+    for (int i = (x+1); i < n; i++) {
+        if (board[i][y] == '6') break;
+        if (board[i][y] == '0') {
+            board[i][y] = '7';
+        }
+    }
+
+    for (int i = (y-1); i > -1; i--) {
+        if (board[x][i] == '6') break;
+        if (board[x][i] == '0') {
+            board[x][i] = '7';
+        }
+    }
+
+    for (int i = (x-1); i > -1; i--) {
+        if (board[i][y] == '6') break;
+        if (board[i][y] == '0') {
+            board[i][y] = '7';
+        }
+    }
+}
+
+void cctv_draw(int x, int y, int dir, char cctv) {
+    switch(cctv) {
+        case '1': 
+            cctv_1_draw(x, y, dir);
+            break;
+        case '2':
+            cctv_2_draw(x, y, dir);
+            break;
+        case '3':
+            cctv_3_draw(x, y, dir);
+            break;
+        case '4':
+            cctv_4_draw(x, y, dir);
+            break;
+    }
 }
 
 int main(void)
@@ -171,10 +218,10 @@ int main(void)
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
             cin >> board[i][j];
-            if (board[i][j] == '1') cctv1_pos.push({i, j});
-            else if (board[i][j] == '2') cctv2_pos.push({i, j});
-            else if (board[i][j] == '3') cctv3_pos.push({i, j});
-            else if (board[i][j] == '4') cctv4_pos.push({i, j});
+            if (board[i][j] == '1') cctv_pos.push_back(make_tuple(i, j, '1'));
+            else if (board[i][j] == '2') cctv_pos.push_back(make_tuple(i, j, '2'));
+            else if (board[i][j] == '3') cctv_pos.push_back(make_tuple(i, j, '3'));
+            else if (board[i][j] == '4') cctv_pos.push_back(make_tuple(i, j, '4'));
             else if (board[i][j] == '5') cctv5_pos.push({i, j});
             else ;
         }
@@ -185,7 +232,22 @@ int main(void)
         cctv_5_draw(cctv5.first, cctv5.second, 0);
     }
 
-    
-    
+    int size = cctv_pos.size();
+    // cout << "cctv_pos size: " << size << "\n";
 
+    for (int tmp = 0; tmp < pow(4, size); tmp++) {
+        int brute = tmp;
+        for (int i = 0; i < size; i++) {
+            tuple<int, int, char> cur = cctv_pos[i];
+            cctv_draw(get<0>(cur), get<1>(cur), (brute%4), get<2>(cur));
+            brute /= 4;
+        }
+        // board_print();
+        int cur_min = count_zero();
+        min_value = min_value > cur_min ? cur_min : min_value;
+        // cout << "zero cnt: " << min_value << "\n";
+        board_clear();
+    }
+
+    cout << min_value;
 }
