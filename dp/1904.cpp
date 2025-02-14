ff1d6d1165265ad000f3456877bfc3d288bddca1
mhlee <mhlee@autocrypt.io>
2025-02-14 13:59:17 +0900

    dp-1904

---
+++ dp/1904.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+int d[1000005];
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n;
+    d[1] = 1;
+    d[2] = 2;
+    for (int i = 3; i <= n; i++) {
+        d[i] = (d[i-2] + d[i-1]) % 15746;
+    }
+    
+    cout << d[n] << '\n';
+}
