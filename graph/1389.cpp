ccaae9f8f1e34034b0fc7963607ee004480bbe5a
mhlee <mhlee@autocrypt.io>
2025-02-19 22:21:56 +0900

    graph-1389

---
+++ graph/1389.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+vector<int> vec[105];
+int dist[105];
+
+void dist_clear() {
+	for (int i = 0; i < 105; i++) dist[i] = -1;
+}
+
+int get_dist_sum() {
+	int sum = 0;
+	for (int i = 1; i <= n; i++) sum += dist[i];
+	return sum;
+}
+
+void bfs(int x) {
+	queue<int> q;
+	q.push(x);
+	dist[x] = 0;
+	while (!q.empty()) {
+		int cur = q.front();
+		q.pop();
+		for (auto nxt : vec[cur]) {
+			if (dist[nxt] >= 0) continue;
+			q.push(nxt);
+			dist[nxt] = dist[cur] + 1;
+		}
+	}
+}
+
+int main(void)
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n >> m;
+	int s = 0;
+	int e = 0;
+	while (m--) {
+		cin >> s >> e;
+		vec[s].push_back(e);
+		vec[e].push_back(s);
+	}
+
+	vector<pair<int, int>> ret;
+
+	for (int i = 1; i <= n; i++) {
+		dist_clear();
+		bfs(i);
+		//cout << get_dist_sum() << '\n';
+		ret.push_back({get_dist_sum(), i});
+	}
+
+	sort(ret.begin(), ret.end());
+
+	cout << ret[0].second << '\n';
+
+	//vector<int> ret_num;
+	//ret_num.push_back(ret[0].second);
+	//int min_v = ret[0].first;
+	//for (int i = 1; i <= n; i++) {
+	//	if (min_v < ret[i].first) break;
+	//	ret_num.push_back(ret[i].second);
+	//}	
+
+	//sort(ret_num.begin(), ret_num.end());
+	//cout << ret_num[0] << '\n';
+}
