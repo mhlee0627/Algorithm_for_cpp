c1a17bc3832f53cb64485dc6a36f046ad8e2f020
mhlee <mhlee@autocrypt.io>
2025-02-20 12:43:44 +0900

    dp-11055

---
+++ dp/11055.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+int arr[1005];
+int d[1005];
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        cin >> arr[i];
+    }   
+
+    d[1] = arr[1];
+    for (int i = 2; i <= n; i++) {
+        int max_v = 0;
+        for (int j = 1; j < i; j++) {
+            if (arr[i] > arr[j]) {
+                max_v = max(max_v, d[j]);
+            }
+
+            d[i] = max_v + arr[i];
+        }
+    }
+
+    sort(d, d+n+1);
+
+    cout << d[n] << '\n';
+}
