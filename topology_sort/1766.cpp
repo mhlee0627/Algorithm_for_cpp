699b1fda9174e657776722a9a72401fd81389356
mhlee <mhlee@autocrypt.io>
2025-05-15 22:42:18 +0900

    t_sort-1766

---
+++ topology_sort/1766.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+vector<int> adj[32005];
+int deg[32005];
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n >> m;
+    int a, b;
+    for (int i = 0; i < m; i++) {
+        cin >> a >> b;
+        adj[a].push_back(b);
+        deg[b]++;
+    }
+
+    priority_queue<int, vector<int>, greater<int>> q;
+    vector<int> ret;
+
+    for (int i = 1; i <= n; i++) {
+        if (deg[i] == 0) q.push(i);
+    }
+
+    while (!q.empty()) {
+        int cur = q.top();
+        q.pop();
+        ret.push_back(cur);
+
+        for (auto nxt : adj[cur]) {
+            deg[nxt]--;
+            if (deg[nxt] == 0) q.push(nxt);
+        }
+    }
+
+    for (auto elem : ret) {
+        cout << elem << ' ';
+    }
+    cout << '\n';
+}
